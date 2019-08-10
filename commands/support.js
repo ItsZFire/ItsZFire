e67b42365d03c02b715e394bcce617c046882450
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-08-10 16:07:04 +0700

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -7,10 +7,10 @@ module.exports.run = async (bot, message, args) => {
     .setAuthor(message.author.username, message.author.displayAvatarURL)
     .setThumbnail(bicon)
     .setTitle(":information_source: **Support Info**")
-    .addField(":robot: **See all of the commands, Use :**", "n!help")
-    .addField("<:plus:557903316496154645> **Want to invite Nexsiz?, Use :**", "n!invite")
-    .addField(":inbox_tray: **Found some bug?, Use :**", "n!contact")
-    .setFooter("Support Server - (https://discord.gg/mjypjna)")
+    .addField(":robot: **See all of the commands, Use :**", "z!help")
+    .addField("<:plus:557903316496154645> **Want to invite ItsZFire?, Use :**", "z!invite")
+    .addField(":inbox_tray: **Found some bug?, Use :**", "z!contact")
+    .setFooter("Support Server - (https://discord.gg/Jf6yXkV)")
     .setTimestamp()
 
     message.channel.send(supportembed);

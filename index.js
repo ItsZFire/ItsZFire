6e53c44ff1d28fb17366ee9cfd655bd80a8e4fff
ItsZFire <53964802+ItsZFire@users.noreply.github.com>
2019-08-10 14:58:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = 'n!';
+  let prefix = 'z!';
   if (!message.content.startsWith(prefix)) return;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];

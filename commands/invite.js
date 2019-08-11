d9abfb96e996e323d37afc7db16a37e3c1e02f81
ItsZFire <53964802+ItsZFire@users.noreply.github.com>
2019-08-11 08:00:04 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,10 +6,10 @@ module.exports.run = async (bot, message, args) => {
   let inviteembed = new Discord.RichEmbed()
   .setColor("#4286f4")
   .setAuthor(bname, bicon)
-  .setURL("https://discordapp.com/oauth2/authorize?client_id=552076002101297153&scope=bot&permissions=2146954495")
+  .setURL("")
   .setDescription("<:OOFthumbsup:452254913427275777> **Want to invite ItsZFire?, Just click at up there!**")
   .setTitle(":arrow_right: Click here to invite ItsZFire in to your server! <:plus_1:557908827840970773>")
-  .setFooter("Made by ItsZFire","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
+  .setFooter("Made by ItsZFire","")
   .setTimestamp();
 
   message.channel.send(inviteembed);

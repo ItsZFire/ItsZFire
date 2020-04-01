cc5b867d39a194c7efe175dd171901a9f49469ee
ItsZFire <53964802+ItsZFire@users.noreply.github.com>
2020-04-01 20:20:41 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -10,11 +10,11 @@ module.exports.run = async (bot, message, args) => {
     .setAuthor(bname, bicon)
     .setURL(``)
     .setTimestamp()
-    .setTitle(`Click here to join ItsZFire discord server`)
+    .setTitle(`Click here to join Next RK Studio discord server`)
     .setDescription("Made with <:nodejs:553212483415048221> Node.js and <:discordjs:553213251488317440> Discord.js\nHosted bot for free by <:heroku:553414334156374016> Heroku")
     .addField(`:clipboard: Created by`, `5831#5831`, true)
     .addField(`:information_source: Version`, `${version}`, true)
-    .addField(`:tools: Debug`, `Updated to version ${version}, new commands - Fun Commands!\To invite ItsZFire in to your server! type "z!invite"\nCheck out try z!help in the chat!`)
+    .addField(`:tools: Debug`, `Updated to version ${version}, new commands - Fun Commands!\To invite Next RK Studio in to your server! type "z!invite"\nCheck out try z!help in the chat!`)
     .addField(`:robot: Bot Created Date`, bot.user.createdAt)
     .setFooter("Made by ItsZFire","https://cdn.discordapp.com/avatars/390301501165404165/0af5081f5d86ac12831dfa4dd008e90b.png")
     .setTimestamp();
